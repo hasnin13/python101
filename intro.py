777279dea81e77dfc86d8bc2634cdaaa39b115f6
hasnin13 <hanna05101021@student.ndub.edu.bd>
2025-02-16 21:14:32 +0600

    .

---
+++ intro.py
@@ -0,0 +1,19 @@
+#output 
+print("Hello world")
+
+##variable and array
+name = "Hannah"
+print(name)
+
+##array declaration
+cars = ["BMW","HERLEY"]
+print(cars[-1])
+
+
+#Coditionals
+
+if 5>10:
+    print("5 is greater than 10")
+    
+else:
+    print("5 is lower than 10")
